98602d8bcca68be0f4e56754818d6237c296e61e
vagrant <vagrant@localhost.localdomain>
2020-05-02 11:14:35 +0000

    [Finish task_3_Bookers]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: {minimum: 2, maximum: 20}
-  validates :email, presence: true, uniqueness: true
-
+  validates :email, presence: true
   validates :introduction, length: {maximum: 50}
 
   
