9c170f9380e421c997c591ca25019a95ef38d817
飯田恭子 <iidakyouko@mba.local>
2020-06-02 02:18:21 +0900

    コメント機能作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,5 +24,6 @@ class User < ApplicationRecord
   attachment :profile_image 
 
   has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-  
   root 'home#top'
   get 'home/about'
-
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 
-  
-
   resources :users, only: [:show, :edit, :index,:update]
-
-  resources :books, only: [:show, :edit, :index, :update, :create, :destroy]
+  
+  resources :books, only: [:show, :edit, :index, :update, :create, :destroy] do
+    resources :book_comments, only: [:create, :destroy]
+  end
 
 
 end
